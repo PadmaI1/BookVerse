5c8798960a666dfaad9bf4a5511d2743c4f4aa74
PadmaI1 <padmaiyer01@protonmail.com>
2026-07-19 16:43:22 +0530

    update config with email and razorpay settings

--- config.py
+++ config.py
@@ -12,4 +12,19 @@ class Config:
 
 # ✅ DATABASE CONFIG
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS"))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Mail
+    BASE_URL = "https://primate-creamlike-overlaid.ngrok-free.dev"
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
+
+    #Payment Gateway
+    RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+    RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
+
+    RAZORPAY_WEBHOOK_SECRET = os.getenv("RAZORPAY_WEBHOOK_SECRET")
